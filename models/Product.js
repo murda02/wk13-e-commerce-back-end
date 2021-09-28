5d4d0317ccbaac42b793e598c7ae873f7ec0b2d6
Dave Murphy <dave.murphy@plusonerobotics.com>
2021-09-28 15:51:35 -0600

    added working code!

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,12 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-      id: {
+    id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,40 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  //maybe not delete oncascade
+  onDelete: 'CASCADE',
+
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    // unique: false
+  },
+  // as: 'product_tags',
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    // unique: false
+  },
+  // as: 'tag_products',
+  foreignKey: 'tag_id'
+})
+
+
 
 module.exports = {
   Product,
